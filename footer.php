b27dd8969b0fd7b38d4f7b577534d548d4363bde
Gaurav Pareek <me@grv.rocks>
2015-12-08 15:23:33 +0530

    Initial commit from underscores

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package RTCamp_Assignment_thems
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'grvrulz-rtcamp-assignment' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'grvrulz-rtcamp-assignment' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'grvrulz-rtcamp-assignment' ), 'grvrulz-rtcamp-assignment', '<a href="http://grv.rocks" rel="designer">Gaurav Pareek</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
